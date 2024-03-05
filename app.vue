471001aa90765b0329a28f261ce02adc03aed3e7
prxncxss03 <pocon.evangeline@gmail.com>
2024-03-05 15:19:38 +0800

    nuxt primevue tailwind starter with sf font

---
+++ app.vue
@@ -0,0 +1,27 @@
+<script setup>
+  useHead ({
+    meta: [
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0'
+      },
+    ],
+    link: [
+      {
+        rel: 'stylesheet',
+        href: "http://fonts.cdnfonts.com/css/sf-pro-display"
+      }
+    ]
+  })
+</script>
+
+<template >
+  <div class="font-sf">
+    <NuxtPage/>
+  </div>
+</template>
+
+
+<style>
+  @import url('/assets/css/global.css');
+</style>
