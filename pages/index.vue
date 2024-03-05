26190ae40ce31ab2cb312ee1d2e93a80eac16901
prxncxss03 <pocon.evangeline@gmail.com>
2024-03-05 15:31:50 +0800

    modify index

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <Card>
         <template #title> Simple Card </template>
         <template #content>
-            <p class="m-0 font-sf">
+            <p class="m-0">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
                 quas!
             </p>
